2c69fc53622214b10344481120168f6e66371fd2
PutuGdeUKDW <93354513+PutuGdeUKDW@users.noreply.github.com>
2023-12-20 21:09:24 +0700

    Update run_config.sh

--- run_config.sh
+++ run_config.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-# Install Docker
 sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker version
 
-# Add the user to the docker group
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 
-# Install Docker Compose
 curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o docker-compose
 chmod +x docker-compose
 sudo mv docker-compose /usr/bin/
 
-docker-compose --version
 mkdir tugas
 curl -O https://raw.githubusercontent.com/PutuGdeUKDW/CloudProject/main/docker-compose.yml
 curl -O https://github.com/PutuGdeUKDW/CloudProject/blob/main/nginx.conf
